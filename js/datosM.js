5992f26b564d96cd0cf1873c461c5fdb27e35c2e
muchachamba <alexmico849@gmail.com>
2024-11-15 22:12:36 +0100

    Add files via upload

---
+++ js/datosM.js
@@ -0,0 +1,12 @@
+function aaa(){
+	var nn=document.getElementById('aa').value;
+	var pp=document.getElementById('bb').value;
+	if(nn=="alex"){
+		if(pp=="alex123"){
+		alert("correcto");
+		location.href="menuprincipal.html";
+	}
+	else
+		alert("incorrecto");
+}
+}
